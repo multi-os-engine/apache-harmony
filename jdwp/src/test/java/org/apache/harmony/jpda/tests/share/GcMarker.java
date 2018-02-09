e2a06b0c95582ff8c4a6029aadfdd1560b547461
Aart Bik <ajcbik@google.com>
2018-02-08 16:15:23 -0800

    Refine GcMarker to wait for finalization.
    Rationale:
    Although far from elegant, the refinements in GcMarker
    should now guarantee that finalization has been completed.
    
    Test: org.apache.harmony.jpda.tests.jdwp
    
    Bug: 70958370
    
    Change-Id: Ia45742b8452f896ec328820df2b5d7301569c31f

--- jdwp/src/test/java/org/apache/harmony/jpda/tests/share/GcMarker.java
+++ jdwp/src/test/java/org/apache/harmony/jpda/tests/share/GcMarker.java
@@ -18,21 +18,41 @@
 
 package org.apache.harmony.jpda.tests.share;
 
-import java.lang.ref.WeakReference;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
 
 /**
- * A class that allows one to observe GCs.
+ * A class that allows one to observe GCs and finalization.
  */
 public class GcMarker {
-  private Object marker;
-  private WeakReference<Object> markerRef;
+
+  private static int mCount = 0;
+
+  /**
+   * Sentinel object with explicit finalizer
+   * to avoid optimizations around empty finalizer.
+   */
+  private static class Sentinel {
+    Sentinel() { mCount++; }
+    protected void finalize() { mCount--; }
+  }
+
+  private final ReferenceQueue mQueue;
+
+  private Sentinel marker;
+  private PhantomReference<Sentinel> markerRef;
 
   public GcMarker() {
+    mQueue = new ReferenceQueue();
     reset();
   }
 
   private boolean isLive() {
-    return markerRef.get() != null;
+    // Pedantically check mCount as well once the phantom reference
+    // link is enqueued to make sure we have really finalized (since
+    // there is only a single finalizer thread, this means all other
+    // finalizers are finished too).
+    return !markerRef.isEnqueued() || mCount > 0;
   }
 
   private void allowCollection() {
@@ -40,19 +60,35 @@ public class GcMarker {
   }
 
   private void reset() {
-    marker = new Object();
-    markerRef = new WeakReference<Object>(marker);
+    marker = new Sentinel();
+    markerRef = new PhantomReference<Sentinel>(marker, mQueue);
   }
 
   public void waitForGc() {
+    // Start with a full collection to discourage minor collections in the middle of this.
+    // Minor collections could cause us to collect the Sentinel but not older objects.
+    Runtime.getRuntime().gc();
+
+    // Release the sentinel.
     allowCollection();
-    // Requests GC and finalization of objects.
+
+    // Another collection.
+    Runtime.getRuntime().gc();
+
+    // Request finalization of objects, and subsequent reference enqueueing.
     do {
-        try { Thread.sleep(10); } catch (Exception e) {}
-        System.gc();
         System.runFinalization();
-        System.gc();
+        Runtime.getRuntime().gc();
+        try { Thread.sleep(10); } catch (Exception e) {}
     } while (isLive());
+
+    // Remove the phantom reference from the queue and reset the only
+    // remaining reference to the phantom reference to break the
+    // sentinel's "phantom reachability".
+    try { mQueue.remove(); } catch (Exception e) {}
+    markerRef = null;
+
+    // Prepare for the next waitForGc().
     reset();
   }
 }
